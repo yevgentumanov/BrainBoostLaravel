dcb0666f610428080392e0692aef35b8cf31c98c
yevgentumanov <strela3@gmail.com>
2023-04-25 23:10:10 +0200

    migration test
    migration test

--- brainboost/public/index.php
+++ brainboost/public/index.php
@@ -3,3 +3,4 @@ echo '<h1>Pagina en construccion!!</h1>';
 echo '<h1>Pronto estaremos online</h1>';
 echo '<h1>Ahora mismo estamos preparando la BD</h1>';
 echo '<h1>Gracias por su paciencia</h1>';
+echo '<h1>Una frase mas...</h1>';

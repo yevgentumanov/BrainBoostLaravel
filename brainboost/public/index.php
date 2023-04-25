2f1e0f00ded31ac82844b21855893978bbbcd72f
yevgentumanov <strela3@gmail.com>
2023-04-25 22:13:08 +0200

    Update index.php

--- brainboost/public/index.php
+++ brainboost/public/index.php
@@ -1,4 +1,5 @@
 <?php
 echo '<h1>Pagina en construccion!!</h1>';
 echo '<h1>Pronto estaremos online</h1>';
+echo '<h1>Ahora mismo estamos preparando la BD</h1>';
 echo '<h1>Gracias por su paciencia</h1>';

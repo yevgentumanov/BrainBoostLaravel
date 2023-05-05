be92a64a738ff0702d08887b3ae91f36adb257b0
sfbluebell <5634224@alu.murciaeduca.es>
2023-05-06 00:13:05 +0200

    Ruta index

--- brainboost/routes/web.php
+++ brainboost/routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 Route::get('/materia', function () {
     return view('materia');

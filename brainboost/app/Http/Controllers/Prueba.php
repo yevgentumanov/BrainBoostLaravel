302b2479e186b717d0ec74b7b7d96fa43c55b4aa
sfbluebell <5634224@alu.murciaeduca.es>
2023-05-06 16:01:09 +0200

    Modificacion Prueba.php Modifico shell_execute

--- brainboost/app/Http/Controllers/Prueba.php
+++ brainboost/app/Http/Controllers/Prueba.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class Prueba extends Controller
 {
     function vistaInicial() {
-        $prueba = shell_exec('git --git-dir="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel/.git" --work-tree="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel" pull');
+        // $prueba = shell_exec('git --git-dir="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel/.git" --work-tree="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel" pull');
+        $prueba = shell_exec("pwd");
         var_dump($prueba);
         
         // return redirect()->route('index');

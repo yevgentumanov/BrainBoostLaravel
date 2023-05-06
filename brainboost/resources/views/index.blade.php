c1ed6cb652aef1924c4543e4aa78aac4110bb351
sfbluebell <5634224@alu.murciaeduca.es>
2023-05-06 12:33:29 +0200

    Otra Prueba sync

--- brainboost/resources/views/index.blade.php
+++ brainboost/resources/views/index.blade.php
@@ -82,7 +82,7 @@
 
         <!-- Links -->
         <ul class="collapse navbar-collapse" id="navbarMaterias">
-          <a class="nav-link" href="{{ route('materia') }}">Inicio</a>
+          <!-- <a class="nav-link" href="{{ route('materia') }}">Inicio</a> -->
           <a class="nav-link" href="{{ route('materia') }}">Artes (Música, Artes Visuales, Teatro)</a>
           <a class="nav-link" href="{{ route('materia') }}">Ciencias Naturales (Biología, Química y Física)</a>
           <a class="nav-link" href="{{ route('materia') }}">Educación Física</a>

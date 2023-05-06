4b0be8de2d7f63981282c677c04beb8d3054af2d
yevgentumanov <strela3@gmail.com>
2023-05-06 16:44:57 +0200

    Update 2023_05_06_120324_create_materias_table.php

--- brainboost/database/migrations/2023_05_06_120324_create_materias_table.php
+++ brainboost/database/migrations/2023_05_06_120324_create_materias_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('materias', function (Blueprint $table) {
+        Schema::create('MATERIAS', function (Blueprint $table) {
             $table->id();
             $table->strin("nombre_materia");
         });

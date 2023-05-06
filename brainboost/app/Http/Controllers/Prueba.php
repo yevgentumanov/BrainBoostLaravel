0b0ebb74fd2a8b7bb35fdff96a90650d1486c9d7
sfbluebell <5634224@alu.murciaeduca.es>
2023-05-06 16:14:23 +0200

    Cambiada variable $repoPath

--- brainboost/app/Http/Controllers/Prueba.php
+++ brainboost/app/Http/Controllers/Prueba.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class Prueba extends Controller
 {
     function vistaInicial() {
-        $repoPath = 'git --git-dir="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel';
+        $repoPath = '/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel';
         chdir($repoPath);
         $comando = "git pull";
         //$comando = 'git --git-dir="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel/.git" --work-tree="/var/www/html/clinicadentalsanandres.com/BrainBoostLaravel" pull';

49bfb4c11e3206e8299c963c0ce3004bbfe39882
JuankCT <hielosecoct@gmail.com>
2023-05-20 22:41:36 +0200

    Cambiado modelo para Materia

--- brainboost/app/Models/Materia.php
+++ brainboost/app/Models/Materia.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class TableName extends Model
 {
     use HasFactory;
+
+    protected $table = 'materia';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_materia',
+        'nombre_test',
+        'descripcion'
+    ];
 }

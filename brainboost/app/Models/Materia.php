d2cc8716fde5998e24ff6bdac91f98b79d2d2427
JuankCT <hielosecoct@gmail.com>
2023-05-20 22:52:04 +0200

    Arreglado ultimo cambio

--- brainboost/app/Models/Materia.php
+++ brainboost/app/Models/Materia.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class TableName extends Model
+class Materia extends Model
 {
     use HasFactory;
 

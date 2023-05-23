2ec8df42688535f5105fe940ad0f34e665da0f05
sfbluebell <5634224@alu.murciaeduca.es>
2023-05-24 00:38:40 +0200

    Restauro el fragment testRecientes.php Cuando os pongáis a hacer estos fragments, hacedlo en unos nuevos, mientras tanto, dejamos los que hay a modo de "dummy"

--- brainboost/resources/views/fragmentos/testsRecientes.blade.php
+++ brainboost/resources/views/fragmentos/testsRecientes.blade.php
@@ -1,96 +1,50 @@
-{{--<section class="row bg-primary m-4 p-4">--}}
-{{--    <div class="col-12 d-flex justify-content-center">--}}
-{{--        <h2>Test realizados recientemente</h2>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="{{ route('test', ['idTest' => 1]) }}">--}}
-{{--                        <!-- El 1 es el id de test. El id de test habrá que generarlo dinámicamente -->--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="#">--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="#">--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="#">--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="#">--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--    <div class="col-2">--}}
-{{--        <article class="border border-box m-2 d-flex justify-content-center bg-light">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-12">--}}
-{{--                    <a class="row d-flex justify-content-center" href="#">--}}
-{{--                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"--}}
-{{--                            alt="logo" style="width:40px;">--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--                <div class="col-12  d-flex justify-content-center">--}}
-{{--                    <h3>Test 1</h3>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    </div>--}}
-{{--</section>--}}
+<section class="row bg-primary m-4 p-4">
+    <div class="col-12 d-flex justify-content-center">
+        <h2>Test realizados recientemente</h2>
+    </div>
+    <div class="col-2">
+        <article class="border border-box m-2 d-flex justify-content-center bg-light">
+            <div class="row">
+                <div class="col-12">
+                    <a class="row d-flex justify-content-center" href="{{ route('test', ['idTest' => 1]) }}">
+                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"
+                            alt="logo" style="width:40px;">
+                    </a>
+                </div>
+                <div class="col-12  d-flex justify-content-center">
+                    <h3>Test 1</h3>
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="col-2">
+        <article class="border border-box m-2 d-flex justify-content-center bg-light">
+            <div class="row">
+                <div class="col-12">
+                    <a class="row d-flex justify-content-center" href="#">
+                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"
+                            alt="logo" style="width:40px;">
+                    </a>
+                </div>
+                <div class="col-12  d-flex justify-content-center">
+                    <h3>Test 1</h3>
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="col-2">
+        <article class="border border-box m-2 d-flex justify-content-center bg-light">
+            <div class="row">
+                <div class="col-12">
+                    <a class="row d-flex justify-content-center" href="#">
+                        <img class="col-12" style="margin: inherit;" src="{!! asset('images/test.png') !!}"
+                            alt="logo" style="width:40px;">
+                    </a>
+                </div>
+                <div class="col-12  d-flex justify-content-center">
+                    <h3>Test 1</h3>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>

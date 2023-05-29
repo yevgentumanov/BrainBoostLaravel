e1118bdbcaeafaecf9d687582f881aec824bd5fb
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-30 00:02:37 +0200

    pruebas con bloques

--- brainboost/resources/views/fragmentos/estadistica.blade.php
+++ brainboost/resources/views/fragmentos/estadistica.blade.php
@@ -5,16 +5,49 @@
                 <div class="card-body bg-primary">
                     <h1>Estadísticas</h1>
                     <section id="general" class="row">
-                        <div type="text" class="col-3" >Nota media:</div>
-                        <div type="text" class="col-3" >Test realizados:</div>
-                        <div type="text" class="col-3" >Materia favorita:</div>
-                        <div type="text" class="col-3" >Última nota:</div>
+                        <div type="text" class="col-3">Nota media: <span class="font-weight-bold">7.35</span></div>
+                        <div type="text" class="col-3">Test realizados: <span class="font-weight-bold">54</span>
+                        </div>
+                        <div type="text" class="col-3">Materia favorita: <span
+                                class="font-weight-bold">Historia</span></div>
+                        <div type="text" class="col-3">Última nota: <span class="font-weight-bold">6.75</span></div>
                     </section>
                     <section id="ultimostests" class="row p-1">
                         <h2>Últimos test realizados</h2>
+                        <div class="rounded bg-light p-3">
+                            <a href="test1.html">Test 1</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test2.html">Test 2</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test3.html">Test 3</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test4.html">Test 4</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test5.html">Test 5</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test6.html">Test 6</a>
+                        </div>
+
                     </section>
                     <section id="masrepetidos" class="row p-1">
                         <h2>Tests con más intentos realizados</h2>
+                        <div class="rounded bg-light p-3">
+                            <a href="test1.html">Test 1</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test2.html">Test 2</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test3.html">Test 3</a>
+                        </div>
+                        <div class="rounded bg-light p-3">
+                            <a href="test4.html">Test 4</a>
+                        </div>
                     </section>
                 </div>
             </div>

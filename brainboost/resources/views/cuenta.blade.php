0a1c6d95c668348b546ad0d1431f70745cbb76ef
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-29 23:25:03 +0200

    Arreglo estructura usuario

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -2,14 +2,13 @@
 
 @section('main')
     <main class="row">
-        <div class="col-12">
-
+        <div class="col-6">
             {{-- Insercion del bloque de cuenta de usuario --}}
             @include('fragmentos.cuenta')
+        </div>
+        <div class="col-6">
             {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
             @include('fragmentos.estadistica')
-
-
         </div>
     </main>
 @endsection

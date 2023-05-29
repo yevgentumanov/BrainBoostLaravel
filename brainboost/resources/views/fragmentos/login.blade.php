be9b1ae751ba40f7ecec6bff5f40f292f15574f5
yevgentumanov <strela3@gmail.com>
2023-05-29 20:56:49 +0200

    Modificacion fragments registro, login y cambio de password

--- brainboost/resources/views/fragmentos/login.blade.php
+++ brainboost/resources/views/fragmentos/login.blade.php
@@ -1,25 +1,36 @@
-<!-- Código Blade para el formulario de inicio de sesión -->
-<form action="{{ route('logintoapp') }}" method="POST">
-    @csrf
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
-    @if(session('warning'))
-        <div class="alert alert-warning">
-            {{ session('warning') }}
-        </div>
-    @endif
-    <div class="form-group">
-        <label for="email">Correo electrónico</label>
-        <input type="email" id="email" name="email" class="form-control" required>
-    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div id="divlogin" class="col-md-6">
+            <div class="card ">
+                <div class="card-body bg-primary">
+                    <form action="{{ route('logintoapp') }}" method="POST">
+                        @csrf
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        @if(session('warning'))
+                            <div class="alert alert-warning">
+                                {{ session('warning') }}
+                            </div>
+                        @endif
+                        <div class="form-group">
+                            <label for="email">Correo electrónico</label>
+                            <input type="email" id="email" name="email" class="form-control" required>
+                        </div>
 
-    <div class="form-group">
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" name="password" class="form-control" required>
+                        <div class="form-group">
+                            <label for="password">Contraseña</label>
+                            <input type="password" id="password" name="password" class="form-control" required>
+                        </div>
+                        <div class="row justify-content-center">
+                            <button type="submit" class="btn btn-success mr-3 ">Iniciar sesión</button>
+                            <button type="submit" class="btn btn-default ml-3 ">Recuperar contraseña</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-</form>
+</div>
--- brainboost/resources/views/fragmentos/registro.blade.php
+++ brainboost/resources/views/fragmentos/registro.blade.php
@@ -1,36 +1,52 @@
 <!-- Código Blade para el formulario de registro -->
-<form action="{{ route('registrar') }}" method="POST">
-    @csrf
-    @if(session('warning'))
-        <div class="alert alert-warning">
-            {{ session('warning') }}
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div id="divregistro" class="col-md-6">
+            <div class="card ">
+                <div class="card-body bg-primary">
+                    <form action="{{ route('registrar') }}" method="POST">
+                        @csrf
+                        @if(session('warning'))
+                            <div class="alert alert-warning">
+                                {{ session('warning') }}
+                            </div>
+                        @endif
+                        <div class="form-group">
+                            <label for="nombre_usuario">Nombre de usuario</label>
+                            <input type="text" id="nombre_usuario" name="nombre_usuario" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Correo electrónico</label>
+                            <input type="email" id="email" name="email" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email_confirmation">Confirmar correo electrónico</label>
+                            <input type="email" id="email_confirmation" name="email_confirmation" class="form-control"
+                                   required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Contraseña</label>
+                            <input type="password" id="password" name="password" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirmar contraseña</label>
+                            <input type="password" id="password_confirmation" name="password_confirmation"
+                                   class="form-control"
+                                   required>
+                        </div>
+
+                        <div class="row justify-content-center">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
-    @endif
-    <div class="form-group">
-        <label for="nombre_usuario">Nombre de usuario</label>
-        <input type="text" id="nombre_usuario" name="nombre_usuario" class="form-control" required>
     </div>
-
-    <div class="form-group">
-        <label for="email">Correo electrónico</label>
-        <input type="email" id="email" name="email" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="email_confirmation">Confirmar correo electrónico</label>
-        <input type="email" id="email_confirmation" name="email_confirmation" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" name="password" class="form-control" required>
-    </div>
-
-    <div class="form-group">
-        <label for="password_confirmation">Confirmar contraseña</label>
-        <input type="password" id="password_confirmation" name="password_confirmation" class="form-control"
-               required>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+</div>

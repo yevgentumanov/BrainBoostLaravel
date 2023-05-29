d9da2818465bace6567a35bc76b330651c3a4602
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-29 23:32:41 +0200

    usu est

--- brainboost/resources/views/fragmentos/estadistica.blade.php
+++ brainboost/resources/views/fragmentos/estadistica.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row justify-content-left">
-        <div id="divregistro" class="col-md-12 p-4">
+        <div id="divregistro" class="col-md-12">
             <div class="card ">
                 <div class="card-body bg-primary">
 

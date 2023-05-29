be3bd8eb316cf47f031132c40822bd5a1adc081f
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-29 23:49:16 +0200

    Mas pruebas

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -2,11 +2,11 @@
 
 @section('main')
     <main class="row">
-        <div class="col-4 m-0 p-4">
+        <div class="col-4 m-0 pt-2 pb-2">
             {{-- Insercion del bloque de cuenta de usuario --}}
             @include('fragmentos.cuenta')
         </div>
-        <div class="col-8 m-0 pt-4 pr-4 pb-4">
+        <div class="col-8 m-0 pt-2 pb-2">
             {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
             @include('fragmentos.estadistica')
         </div>
--- brainboost/resources/views/fragmentos/estadistica.blade.php
+++ brainboost/resources/views/fragmentos/estadistica.blade.php
@@ -6,9 +6,9 @@
                     <h1>Estadísticas</h1>
                     <section id="general" class="row">
                         <div type="text" class="col-3" >Nota media:</div>
-                        <div type="text" class="col-3" >Test realizados:"</div>
-                        <div type="text" class="col-3" >Materia favorita:"</div>
-                        <div type="text" class="col-3" >Última nota:"</div>
+                        <div type="text" class="col-3" >Test realizados:</div>
+                        <div type="text" class="col-3" >Materia favorita:</div>
+                        <div type="text" class="col-3" >Última nota:</div>
                     </section>
                     <section id="ultimostests" class="row p-1">
                         <h2>Últimos test realizados</h2>

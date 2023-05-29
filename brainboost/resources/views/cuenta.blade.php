b4dcf0864fdef02d3e1784555061df224021d95b
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-29 23:53:38 +0200

    Ajuste est usuario

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -2,11 +2,11 @@
 
 @section('main')
     <main class="row">
-        <div class="col-4 m-0 pt-2 pb-2">
+        <div class="col-4 pl-1 pr-0 pt-4 pb-4">
             {{-- Insercion del bloque de cuenta de usuario --}}
             @include('fragmentos.cuenta')
         </div>
-        <div class="col-8 m-0 pt-2 pb-2">
+        <div class="col-8 pl-0 pr-0 pt-4 pb-4">
             {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
             @include('fragmentos.estadistica')
         </div>

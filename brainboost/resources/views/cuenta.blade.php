84271517eff1c34ebea4bc026632ec018b76b613
Juan Carlos <100731447+JuankCT@users.noreply.github.com>
2023-05-29 23:41:51 +0200

    Prueba estadisticas usuario

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -6,7 +6,7 @@
             {{-- Insercion del bloque de cuenta de usuario --}}
             @include('fragmentos.cuenta')
         </div>
-        <div class="col-8 m-0 pl-0 p-4">
+        <div class="col-8 m-0 pt-4 pr-4 pb-4">
             {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
             @include('fragmentos.estadistica')
         </div>
--- brainboost/resources/views/fragmentos/estadistica.blade.php
+++ brainboost/resources/views/fragmentos/estadistica.blade.php
@@ -3,8 +3,19 @@
         <div id="divregistro" class="col-md-12">
             <div class="card ">
                 <div class="card-body bg-primary">
-
                     <h1>Estadísticas</h1>
+                    <section id="general" class="row">
+                        <input type="text" class="col-3" value="Nota media:" />
+                        <input type="text" class="col-3" value="Test realizados:" />
+                        <input type="text" class="col-3" value="Materia favorita:" />
+                        <input type="text" class="col-3" value="Última nota:" />
+                    </section>
+                    <section id="ultimostests" class="row">
+                        <h2>Últimos test realizados</h2>
+                    </section>
+                    <section id="masrepetidos" class="row">
+                        <h2>Tests con más intentos realizados</h2>
+                    </section>
                 </div>
             </div>
         </div>

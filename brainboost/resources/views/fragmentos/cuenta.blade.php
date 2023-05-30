15c6ab6042789c684555c160a959a8779a1c3e4c
JuankCT <hielosecoct@gmail.com>
2023-05-30 21:56:02 +0200

    Foto usuario

--- brainboost/resources/views/fragmentos/cuenta.blade.php
+++ brainboost/resources/views/fragmentos/cuenta.blade.php
@@ -8,28 +8,33 @@
                     <form method="POST" action="{{ route('cambiarpassword') }}">
                         @csrf
 
+                        <div class="row d-flex justify-content-center">
+                            <img class="card-img-top col-5 p-3" src="{!! asset('images/usuario.jfif') !!}"
+                                alt="Card image" style="width:100%">
+                        </div>
+
                         <div class="form-group">
                             <label for="nombre_usuario">Nombre de usuario</label>
                             <input type="text" id="nombre_usuario" name="nombre_usuario"
-                                   value="{{ auth()->user()->nombre_usuario }}" class="form-control" readonly>
+                                value="{{ auth()->user()->nombre_usuario }}" class="form-control" readonly>
                         </div>
 
                         <div class="form-group">
                             <label for="password_actual">Contraseña actual</label>
                             <input type="password" id="password_actual" name="password_actual" class="form-control"
-                                   required>
+                                required>
                         </div>
 
                         <div class="form-group">
                             <label for="nueva_password">Nueva contraseña</label>
                             <input type="password" id="nueva_password" name="nueva_password" class="form-control"
-                                   required>
+                                required>
                         </div>
 
                         <div class="form-group">
                             <label for="confirmar_password">Confirmar contraseña</label>
                             <input type="password" id="confirmar_password" name="confirmar_password"
-                                   class="form-control" required>
+                                class="form-control" required>
                         </div>
 
                         <button type="submit" class="btn btn-primary">Guardar cambios</button>

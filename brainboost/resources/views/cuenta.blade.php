1c5e4f6b2f688fbe1ecc8eef54efda8503800264
JuankCT <hielosecoct@gmail.com>
2023-05-30 22:42:28 +0200

    multiplataforma Usuario

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -2,11 +2,12 @@
 
 @section('main')
     <main class="row">
-        <div class="col-4 pl-1 pr-0 pt-4 pb-4">
+        <div class="col-2 d-none d-md-block d-lg-none"></div>
+        <div class="col-12 col-md-8 col-lg-5 col-xl-4 pl-1 pr-0 pt-4 pb-4">
             {{-- Insercion del bloque de cuenta de usuario --}}
             @include('fragmentos.cuenta')
         </div>
-        <div class="col-8 pl-0 pr-0 pt-4 pb-4">
+        <div class="col-12 col-lg-7 col-xl-8 pl-0 pr-0 pt-4 pb-4">
             {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
             @include('fragmentos.estadistica')
         </div>

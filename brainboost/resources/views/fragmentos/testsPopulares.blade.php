a71ba0509370884e74c38e232050aada55f5ff1a
JuankCT <hielosecoct@gmail.com>
2023-06-01 00:11:26 +0200

    Prueba css

--- brainboost/resources/views/fragmentos/testsPopulares.blade.php
+++ brainboost/resources/views/fragmentos/testsPopulares.blade.php
@@ -2,7 +2,7 @@
     <div class="col-12 d-flex justify-content-center">
         <h2>Test m&aacute;s populares</h2>
     </div>
-    <div class="col-6 col-md-4 col-lg-2">
+    <div class="col-6 col-md-4 col-lg-2 ">
         <article class="border border-box m-2 d-flex justify-content-center bg-light">
             <div class="row">
                 <div class="col-12">

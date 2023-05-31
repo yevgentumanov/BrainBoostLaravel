6a7cc9c63185452f70d607f833aa2509238813f8
JuankCT <hielosecoct@gmail.com>
2023-06-01 00:28:12 +0200

    Prueba css 2

--- brainboost/resources/views/fragmentos/estadistica.blade.php
+++ brainboost/resources/views/fragmentos/estadistica.blade.php
@@ -1,10 +1,12 @@
 <div class="container">
     <div class="row justify-content-left">
         <div id="divregistro" class="col-md-12">
-            <div class="card ">
-                <div class="card-body bg-primary">
-                    <h1>Estadísticas</h1>
-                    <section id="general" class="row">
+
+                <section class="card-body bg-primary p-4">
+                    <div class="mb-4">
+                        <h1>Estadísticas</h1>
+                    </div>
+                    <section id="general" class="row m-2">
                         <div type="text" class="col-3">Nota media: <span class="font-weight-bold">7.35</span></div>
                         <div type="text" class="col-3">Test realizados: <span class="font-weight-bold">54</span>
                         </div>
@@ -12,7 +14,7 @@
                                 class="font-weight-bold">Historia</span></div>
                         <div type="text" class="col-3">Última nota: <span class="font-weight-bold">6.75</span></div>
                     </section>
-                    <section id="ultimostests" class="row p-1">
+                    <div id="ultimostests" class="row m-2 p-4">
                         <h2>Últimos test realizados</h2>
                         <div class="col-12">
                             <div class="row rounded bg-light p-3 mt-1">
@@ -34,8 +36,8 @@
                                 <a href="test6.html">Test 6</a>
                             </div>
                         </div>
-                    </section>
-                    <section id="masrepetidos" class="row p-1">
+                    </div>
+                    <div id="masrepetidos" class="row m-2 p-4">
                         <h2>Tests con más intentos realizados</h2>
                         <div class="col-12">
                             <div class="row rounded bg-light p-3 mt-1">
@@ -51,9 +53,9 @@
                                 <a href="test4.html">Test 4</a>
                             </div>
                         </div>
-                    </section>
-                </div>
-            </div>
+                    </div>
+                </section>
+
         </div>
     </div>
 </div>

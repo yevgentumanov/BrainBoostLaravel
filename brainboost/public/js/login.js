a7d5c77575e745de6d7d8339520f7f04e8454c2a
sfbluebell <5634224@alu.murciaeduca.es>
2023-06-03 16:14:29 +0200

    Controlar eventos Login via JS

---
+++ brainboost/public/js/login.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const divContainerLogin = document.querySelector(".col-12 > .container.superpuesto")
+    const btnLogin = document.querySelector("#btnLogin")
+    const btnCloseLogin = document.querySelector("#divlogin a.exit-card");
+
+    /*-- Realiza modificaciones para que no se usen las rutas de Eugenio para mostrar la ventana de inicio de sesión --*/
+    btnLogin.removeAttribute("href");
+    btnCloseLogin.removeAttribute("href");
+
+    /*-- Controla eventos --*/
+    btnLogin.addEventListener("click", (e) => {
+        divContainerLogin.classList.remove("d-none");
+    });
+    btnCloseLogin.addEventListener("click", (e) => {
+        divContainerLogin.classList.add("d-none")
+    });
+
+});

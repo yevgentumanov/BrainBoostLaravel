8cf0ca6008c61680ef8a598c549e23aead01d182
JuankCT <hielosecoct@gmail.com>
2023-06-05 19:12:39 +0200

    Arreglado fondo usuario

--- brainboost/resources/views/cuenta.blade.php
+++ brainboost/resources/views/cuenta.blade.php
@@ -2,14 +2,16 @@
 
 @section('main')
     <main class="row">
-        <div class="col-2 d-none d-md-block d-lg-none"></div>
-        <div class="col-12 col-md-8 col-lg-5 col-xl-4 pl-1 pr-0 pt-4 pb-4">
-            {{-- Insercion del bloque de cuenta de usuario --}}
-            @include('fragmentos.cuenta')
-        </div>
-        <div class="col-12 col-lg-7 col-xl-8 pl-0 pr-0 pt-4 pb-4">
-            {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
-            @include('fragmentos.estadistica')
+        <div class="row w-100 cuerpo">
+            <div class="col-2 d-none d-md-block d-lg-none"></div>
+            <div class="col-12 col-md-8 col-lg-5 col-xl-4 pl-1 pr-0 pt-4 pb-4">
+                {{-- Insercion del bloque de cuenta de usuario --}}
+                @include('fragmentos.cuenta')
+            </div>
+            <div class="col-12 col-lg-7 col-xl-8 pl-0 pr-0 pt-4 pb-4">
+                {{-- Insercion del bloque de cuenta de estadísticas del usuario --}}
+                @include('fragmentos.estadistica')
+            </div>
         </div>
     </main>
 @endsection

3ca51294caf16bbb4a9823cf928893edbfae19bd
yevgentumanov <strela3@gmail.com>
2023-06-07 23:24:30 +0200

    Update cors.php

--- brainboost/config/cors.php
+++ brainboost/config/cors.php
@@ -15,38 +15,20 @@ return [
     |
     */
 
-        'paths' => [],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
-        'allowed_methods' => [],
+    'allowed_methods' => ['*'],
 
-        'allowed_origins' => ['https://www.clinicadentalsanandres.com'],
+    'allowed_origins' => ['*'],
 
-        'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [],
 
-        'allowed_headers' => [],
+    'allowed_headers' => ['*'],
 
-        'exposed_headers' => [],
+    'exposed_headers' => [],
 
-        'max_age' => 0,
+    'max_age' => 0,
 
-        'supports_credentials' => false,
-
-
-
-//    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-//
-//    'allowed_methods' => ['*'],
-//
-//    'allowed_origins' => ['*'],
-//
-//    'allowed_origins_patterns' => [],
-//
-//    'allowed_headers' => ['*'],
-//
-//    'exposed_headers' => [],
-//
-//    'max_age' => 0,
-//
-//    'supports_credentials' => false,
+    'supports_credentials' => false,
 
 ];

dcce9036236d8ce21124a2bc5bab36f6f127bda7
JuankCT <hielosecoct@gmail.com>
2023-06-11 22:50:08 +0200

    Fondo cuerpo historial

--- brainboost/resources/views/historialTestRealizados.blade.php
+++ brainboost/resources/views/historialTestRealizados.blade.php
@@ -1,7 +1,7 @@
 @extends('plantillas.base')
 
 @section('main')
-    <main class="row w-100 m-0">
+    <main class="row w-100 m-0 cuerpo">
         <div class="col-12 pl-0 pr-0 pt-4 pb-4">
             {{-- Fragmento con el historial de todos los test realizados --}}
             @include('fragmentos.historialIntentos')

61e0fddd6f1d6a13e7a3012582580350e99f0a9d
JuankCT <hielosecoct@gmail.com>
2023-06-11 22:52:39 +0200

    Correccion cambio cuerpo

--- brainboost/resources/views/historialTestRealizados.blade.php
+++ brainboost/resources/views/historialTestRealizados.blade.php
@@ -1,8 +1,8 @@
 @extends('plantillas.base')
 
 @section('main')
-    <main class="row w-100 m-0 cuerpo">
-        <div class="col-12 pl-0 pr-0 pt-4 pb-4">
+    <main class="row w-100 m-0">
+        <div class="col-12 pl-0 pr-0 pt-4 pb-4 cuerpo">
             {{-- Fragmento con el historial de todos los test realizados --}}
             @include('fragmentos.historialIntentos')
         </div>

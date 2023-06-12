00e2d6a1da49958e8cf206537edcc40db1ba52fe
JuankCT <hielosecoct@gmail.com>
2023-06-13 01:19:37 +0200

    Arreglado verify correo

--- brainboost/resources/views/auth/verify-email.blade.php
+++ brainboost/resources/views/auth/verify-email.blade.php
@@ -6,18 +6,18 @@
 
             <div class="container">
                 <div class="row justify-content-center">
-                    <div id="divregistro" class="col-md-12">
+                    <div id="divregistro" class="col-md-12 s-materia">
                         <div class="card pb-2">
                             <div id="v-texto" class="card-body bg-primary">
                                 <h1>Verificacion de correo</h1>
                                 <div class="col-12">
-                                    <h3>¡Gracias por registrarte! Antes de comenzar, ¿podrías verificar tu dirección de
+                                    <p>¡Gracias por registrarte! Antes de comenzar, ¿podrías verificar tu dirección de
                                         correo
                                         electrónico haciendo clic en el enlace que te acabamos de enviar por correo? Si
                                         no recibiste
                                         el correo electrónico, estaremos encantados de enviarte otro. ¡Bienvenido/a al
                                         proyecto
-                                        BrainBoost.es!</h3>
+                                        BrainBoost.es!</p>
                                 </div>
                             </div>
                             <div class="mt-4 row justify-content-center">
@@ -25,7 +25,7 @@
                                     @csrf
 
                                     <div>
-                                        <button type="submit" class="btn btn-light btn-5 px-4 py-2 rounded-md">Volver a enviar
+                                        <button type="submit" class="btn btn-5 px-4 py-2  mb-2 rounded-md">Volver a enviar
                                         </button>
                                     </div>
                                 </form>
@@ -33,7 +33,7 @@
                                 <form method="POST" action="{{ route('salir') }}">
                                     @csrf
 
-                                    <button type="submit" class="bg-red-500 px-4 py-2 ml-2 rounded-md"> Salir
+                                    <button type="submit" class="btn btn-5 px-4 py-2 ml-2 mb-2 rounded-md"> Salir
                                     </button>
                                 </form>
                             </div>

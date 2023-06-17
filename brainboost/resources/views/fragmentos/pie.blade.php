3bf11d3b77815026fd91395f1619b49386243ea5
sfbluebell <5634224@alu.murciaeduca.es>
2023-06-17 12:47:11 +0200

    Cambios pie licencia creative commons

--- brainboost/resources/views/fragmentos/pie.blade.php
+++ brainboost/resources/views/fragmentos/pie.blade.php
@@ -1,8 +1,8 @@
 <footer class="row m-0 bg-dark">
-    <div id="f-links" class="col-12 col-md-6 col-xl-4 order-md-2 pie-paginas p-3">
+    <div id="f-links" class="col-12 col-md-6 col-lg-3 order-md-2 pie-paginas p-3">
         <div class="row justify-content-center p-1">
             <a href="#">
-                <h6 class="row w-100">WWW.BRAINBOOST.COM</h6>
+                <h6 class="row w-100">WWW.BRAINBOOST.ES</h6>
             </a>
         </div>
         <div class="row">
@@ -12,17 +12,28 @@
             <a class="col-12" href="https://www.w3schools.com/">W3Schools</a>
         </div>
         <div class="row">
-            <a class="col-12" href="https://github.com/">GitHub</a>
+            <a class="col-12" href="https://github.com/yevgentumanov/BrainBoostLaravel">GitHub</a>
         </div>
         <div class="row">
             <a class="col-12" href="https://animista.net/">animista</a>
         </div>
     </div>
-    <div id="f-info" class="col-12 col-md-6 col-xl-2 order-md-3 p-3">
+    <div id="f-info" class="col-12 col-md-6 col-lg-3 order-md-3 p-3">
         <div class="row justify-content-center">
             <h6>INFORMACI&Oacute;N</h6>
         </div>
         <div class="row">
+            <a href="https://creativecommons.org/licenses/by/4.0/deed.es" class="col-12 text-center">
+                <img src="{!! asset('images/CC_BY-SA_icon.png') !!}" alt="Licencia Creative Commons">
+                <br>
+                Santiago Francisco San Pablo Raposo
+                <br>
+                Juan Carlos Mendoza Saucedo
+                <br>
+                Yevgen Tumanov Kondratyeva 
+            </a>
+        </div>
+        {{-- <div class="row">
             <a href="" class="col-12 fa fa-clock-o p-1">&nbsp;09:00 - 21:00 (24/7)</a>
         </div>
         <div class="row">
@@ -34,11 +45,11 @@
         {{-- <div class="row">
             <a href="" class="col-12 fa fa-fax p-1">&nbsp;+34 968 26 26 34</a>
         </div> --}}
-        <div class="row">
+        {{-- <div class="row">
             <a href="" class="col-12 fa fa-envelope-o p-1">&nbsp;info&#64;brainboost.es</a>
-        </div>
+        </div> --}}
     </div>
-    <div id="f-redes" class="col-12 col-md-6 col-xl-3 order-md-4 p-3" style="display: grid;">
+    <div id="f-redes" class="col-12 col-md-6 col-lg-3 order-md-4 p-3" style="display: grid;">
         <div class="row justify-content-center p-1">
             <h6>NUESTRAS REDES HABLAN</h6>
         </div>
@@ -56,7 +67,7 @@
             <h6>S&Iacute;GUENOS, TE GUSTAR&Aacute;</h6>
         </div>
     </div>
-    <div id="f-logo" class="col-12 col-md-6 col-xl-3 order-md-1 d-flex align-items-center">
+    <div id="f-logo" class="col-12 col-md-6 col-lg-3 order-md-1 d-flex align-items-center">
         <img src="{!! asset('images/Logo_letras_chicas_con_sombra en blanco y negro-trayectos-v3.svg') !!}" height="150px" class="d-none mx-auto d-md-block"
             style="width: 100%; height: fit-content;" alt="Logo">
         <img src="{!! asset('images/Logo_letras_chicas_con_sombra en blanco y negro-trayectos-v3.svg') !!}" height="100px" class="mx-auto d-md-none"
